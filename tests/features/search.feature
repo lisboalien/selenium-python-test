7ca8d4f7565e022cd2e8284fddaafd75ebc746ad
Aline de Farias Lisboa <lisboalien@gmail.com>
2020-09-12 22:00:34 -0300

    Adding a Scenario Outline example

--- tests/features/search.feature
+++ tests/features/search.feature
@@ -3,8 +3,19 @@ Feature: Searching for a term in DuckDuckGo page
   Background: Opening DuckDuckGo home page
     Given the DuckDuckGo home page is displayed
 
+
   Scenario: Search DuckDuckGo
     Given the user searches for "panda"
     Then the search result query is "panda"
     And the search result links pertain to "panda"
     And the search result title contains "panda"
+
+
+  Scenario Outline: Search for the different phrases
+    Given the user searches for "<initial>"
+    Then the search result query, link and title is like the searched term
+
+    Examples:
+      | initial  |
+      | duck     | 
+      | football |
--- tests/steps_defs/test_search_steps.py
+++ tests/steps_defs/test_search_steps.py
@@ -15,32 +15,43 @@ scenarios('../features/search.feature')
 
 # Given Steps
 
-@given(parsers.parse('the user searches for "{text}"'))
-@given(parsers.parse('the user searches for the phrase:\n"""{text}"""'))
-def search_phrase(browser, text):
+@given(parsers.parse('the user searches for "{initial}"'))
+@given(parsers.parse('the user searches for the phrase:\n"""{initial}"""'))
+@given('the user searches for "<initial>"')
+def search_phrase(browser, initial):
     search_page = DuckDuckGoSearchPage(browser)
-    search_page.search(text)
+    search_page.search(initial)
 
 
 # Then Steps
 
-@then(parsers.parse('the search result query is "{phrase}"'))
-def results_have_one(browser, phrase):
+@then(parsers.parse('the search result query is "{result}"'))
+@then('the search result query is "<result>"')
+def results_have_one(browser, result):
     result_page = DuckDuckGoResultPage(browser)
-    assert phrase == result_page.search_input_value()
+    assert result == result_page.search_input_value()
+
+
+@then('the search result query, link and title is like the searched term')
+def check_search_results(browser, initial):
+    results_have_one(browser, initial)
+    result_links(browser, initial)
+    result_text(browser, initial)
 
 
 # And Steps
 
-@then(parsers.parse('the search result links pertain to "{phrase}"'))
-def result_links(browser, phrase):
+@then(parsers.parse('the search result links pertain to "{phrases}"'))
+@then('the search result links pertain to "<phrases>"')
+def result_links(browser, phrases):
     result_page = DuckDuckGoResultPage(browser)
     titles = result_page.result_link_titles()
-    matches = [t for t in titles if phrase.lower() in t.lower()]
+    matches = [t for t in titles if phrases.lower() in t.lower()]
     assert len(matches) > 0
 
 
 @then(parsers.parse('the search result title contains "{phrase}"'))
+@then('the search result links pertain to "<phrase>"')
 def result_text(browser, phrase):
     result_page = DuckDuckGoResultPage(browser)
     assert phrase in result_page.title()
