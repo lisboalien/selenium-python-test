be583b899c5d96ae5b98d2eeb47f91d8de760ecc
Aline de Farias Lisboa <lisboalien@gmail.com>
2020-09-30 22:55:00 -0300

    Spliting the locators in List, Input, Button and Other locators

--- pages/result.py
+++ pages/result.py
@@ -12,26 +12,33 @@ from pages.common import CommonPage
 class DuckDuckGoResultPage(CommonPage):
     # Locators
 
-    RESULT_LINKS = (By.CSS_SELECTOR, 'a.result__a')
-    RESULT_IMAGES = (By.CSS_SELECTOR, 'span.tile--img__title')
-    RESULT_VIDEOS = (By.XPATH, '//h6/a')
-    RESULT_NEWS = (
+    # Lists Locators
+    LIST_RESULT_LINKS = (By.CSS_SELECTOR, 'a.result__a')
+    LIST_RESULT_IMAGES = (By.CSS_SELECTOR, 'span.tile--img__title')
+    LIST_RESULT_VIDEOS = (By.XPATH, '//h6/a')
+    LIST_RESULT_NEWS = (
         By.XPATH, '//div[contains(@class, "result--news")]//h2/a[1]')
-    SEARCH_INPUT = (By.ID, 'search_form_input')
-    SEARCH_BUTTON = (By.ID, 'search_button')
-    MORE_RESULTS_BUTTON = (By.CSS_SELECTOR, 'a.result--more__btn')
-    IMAGES_BUTTON = (By.CSS_SELECTOR, 'a.js-zci-link--images')
-    VIDEOS_BUTTON = (By.CSS_SELECTOR, 'a.js-zci-link--videos')
-    NEWS_BUTTON = (By.CSS_SELECTOR, 'a.js-zci-link--news')
-    RESULT_PER_REGION_SWITCH = (
+    LIST_COUNTRIES = (By.XPATH, '//a[contains(@class, "modal__list__link")]')
+    LIST_SITE_ICONS = (By.CSS_SELECTOR, 'span.result__icon')
+
+    # Input Locators
+    TEXTBOX_SEARCH_INPUT = (By.ID, 'search_form_input')
+
+    # Button Locators
+    BUTTON_SEARCH = (By.ID, 'search_button')
+    BUTTON_MORE_RESULTS = (By.CSS_SELECTOR, 'a.result--more__btn')
+    BUTTON_IMAGES = (By.CSS_SELECTOR, 'a.js-zci-link--images')
+    BUTTON_VIDEOS = (By.CSS_SELECTOR, 'a.js-zci-link--videos')
+    BUTTON_NEWS = (By.CSS_SELECTOR, 'a.js-zci-link--news')
+    BUTTON_REGION_DROPDOWN = (By.CSS_SELECTOR, 'div.dropdown--region > a')
+    BUTTON_SETTINGS = (By.CSS_SELECTOR, 'div.dropdown--settings')
+    SWITCH_SITE_ICONS = (By.XPATH, '//label[@for="setting_kf"]')
+    SWITCH_RESULT_PER_REGION = (
         By.CSS_SELECTOR, 'div.js-region-filter-switch > span')
-    REGION_DROPDOWN_BUTTON = (By.CSS_SELECTOR, 'div.dropdown--region > a')
-    COUNTRIES_LIST = (By.XPATH, '//a[contains(@class, "modal__list__link")]')
-    SETTINGS_BUTTON = (By.CSS_SELECTOR, 'div.dropdown--settings')
-    SITE_ICONS_CHECKBOX = (By.XPATH, '//label[@for="setting_kf"]')
-    SITE_ICONS_CONFIG_VALUE = (
+
+    # Other Locators
+    VALUE_SITE_ICONS_CONFIG = (
         By.XPATH, '//div[contains(@class, "js-settings-dropdown-appearance")]/div[2]')
-    SITE_ICONS = (By.CSS_SELECTOR, 'span.result__icon')
 
     # Initializer
 
@@ -41,81 +48,81 @@ class DuckDuckGoResultPage(CommonPage):
     # Interaction Element Methods
 
     def set_search_input(self, phrase):
-        search_input = self.browser.find_element(*self.SEARCH_INPUT)
+        search_input = self.browser.find_element(*self.TEXTBOX_SEARCH_INPUT)
         search_input.clear
         search_input.send_keys(phrase)
 
     def get_site_icons_config_value(self):
         value = self.browser.find_element(
-            *self.REGION_DROPDOWN_BUTTON).text
+            *self.BUTTON_REGION_DROPDOWN).text
         return value
 
     def get_search_input_value(self):
         value = self.browser.find_element(
-            *self.SEARCH_INPUT).get_attribute('value')
+            *self.TEXTBOX_SEARCH_INPUT).get_attribute('value')
         return value
 
     def get_selected_country(self):
         value = self.browser.find_element(
-            *self.REGION_DROPDOWN_BUTTON).get_attribute('innerText')
+            *self.BUTTON_REGION_DROPDOWN).get_attribute('innerText')
         return value
 
     def title(self):
         return self.browser.title
 
     def btn_search_click(self):
-        self.browser.find_element(*self.SEARCH_BUTTON).click()
+        self.browser.find_element(*self.BUTTON_SEARCH).click()
 
     def btn_more_results_click(self):
-        self.browser.find_element(*self.MORE_RESULTS_BUTTON).click()
+        self.browser.find_element(*self.BUTTON_MORE_RESULTS).click()
 
     def btn_images_click(self):
-        self.browser.find_element(*self.IMAGES_BUTTON).click()
+        self.browser.find_element(*self.BUTTON_IMAGES).click()
 
     def btn_videos_click(self):
-        self.browser.find_element(*self.VIDEOS_BUTTON).click()
+        self.browser.find_element(*self.BUTTON_VIDEOS).click()
 
     def btn_news_click(self):
-        self.browser.find_element(*self.NEWS_BUTTON).click()
+        self.browser.find_element(*self.BUTTON_NEWS).click()
 
     def btn_settings_click(self):
-        self.browser.find_element(*self.SETTINGS_BUTTON).click()
+        self.browser.find_element(*self.BUTTON_SETTINGS).click()
 
     def chk_site_icons_click(self):
-        self.browser.find_element(*self.SITE_ICONS_CHECKBOX).click()
+        self.browser.find_element(*self.SWITCH_SITE_ICONS).click()
 
     def switch_result_per_region(self):
-        self.browser.find_element(*self.RESULT_PER_REGION_SWITCH).click()
+        self.browser.find_element(*self.SWITCH_RESULT_PER_REGION).click()
 
     def get_select_box_country_list(self):
-        self.browser.find_element(*self.REGION_DROPDOWN_BUTTON).click()
-        countries = self.browser.find_elements(*self.COUNTRIES_LIST)
+        self.browser.find_element(*self.BUTTON_REGION_DROPDOWN).click()
+        countries = self.browser.find_elements(*self.LIST_COUNTRIES)
         return countries
 
     # Interaction Action Methods
 
     def result_link_titles(self):
-        links = self.browser.find_elements(*self.RESULT_LINKS)
+        links = self.browser.find_elements(*self.LIST_RESULT_LINKS)
         titles = [link.text for link in links]
         return titles
 
     def result_image_titles(self):
-        links = self.browser.find_elements(*self.RESULT_IMAGES)
+        links = self.browser.find_elements(*self.LIST_RESULT_IMAGES)
         titles = [link.text for link in links]
         return titles
 
     def result_videos_titles(self):
-        links = self.browser.find_elements(*self.RESULT_VIDEOS)
+        links = self.browser.find_elements(*self.LIST_RESULT_VIDEOS)
         titles = [link.text for link in links]
         return titles
 
     def result_news_titles(self):
-        links = self.browser.find_elements(*self.RESULT_NEWS)
+        links = self.browser.find_elements(*self.LIST_RESULT_NEWS)
         titles = [link.text for link in links]
         return titles
 
     def get_result_links_icons_status(self):
-        icons = self.browser.find_elements(*self.SITE_ICONS)
+        icons = self.browser.find_elements(*self.LIST_SITE_ICONS)
         icons_status = [link.get_attribute('class') for link in icons]
         return icons_status
 
--- pages/search.py
+++ pages/search.py
@@ -18,9 +18,14 @@ class DuckDuckGoSearchPage(CommonPage):
 
     # Locators
 
-    SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
-    SEARCH_BUTTON = (By.ID, 'search_button_homepage')
-    AUTO_COMPLETE_RESULTS = (By.CSS_SELECTOR, 'span.t-normal')
+    # Lists Locators
+    LIST_AUTO_COMPLETE_RESULTS = (By.CSS_SELECTOR, 'span.t-normal')
+
+    # Input Locators
+    TEXTBOX_SEARCH_INPUT = (By.ID, 'search_form_input_homepage')
+
+    # Button Locators
+    BUTTON_SEARCH = (By.ID, 'search_button_homepage')
 
     # Initializer
 
@@ -33,10 +38,10 @@ class DuckDuckGoSearchPage(CommonPage):
         self.browser.get(self.URL)
 
     def set_search_input(self, phrase):
-        self.browser.find_element(*self.SEARCH_INPUT).send_keys(phrase)
+        self.browser.find_element(*self.TEXTBOX_SEARCH_INPUT).send_keys(phrase)
 
     def btn_search_click(self):
-        self.browser.find_element(*self.SEARCH_BUTTON).click()
+        self.browser.find_element(*self.BUTTON_SEARCH).click()
 
     # Interaction Action Methods
 
@@ -45,11 +50,11 @@ class DuckDuckGoSearchPage(CommonPage):
         self.btn_search_click()
 
     def auto_complete_results(self):
-        results = self.browser.find_elements(*self.AUTO_COMPLETE_RESULTS)
+        results = self.browser.find_elements(*self.LIST_AUTO_COMPLETE_RESULTS)
         titles = [result.text for result in results]
         return titles
 
     def auto_complete_term_selection(self):
-        results = self.browser.find_elements(*self.AUTO_COMPLETE_RESULTS)
+        results = self.browser.find_elements(*self.LIST_AUTO_COMPLETE_RESULTS)
         random_result = choice(results)
         random_result.click()
